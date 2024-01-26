069d21751078da2ba98211fc0270d6a4bab6bd0f
Patrycja Ciosek <109743645+xPatiCiosek@users.noreply.github.com>
2024-01-26 21:06:33 +0000

    updated compatibility with html elements
    put logic into function
    added event listener to input

--- app.js
+++ app.js
@@ -1,40 +1,53 @@
+const response = document.querySelector('.response');
+const input = document.getElementById('input');
+const btn = document.getElementById('btn');
 
-let userQuestion = window.prompt('what is your question?')
-let randomNumber = Math.floor(Math.random() *7)
-let eightBall = ''
 
-console.log('Hello!');
+ function assignResponse(){
+  
+      let randomNumber = Math.floor(Math.random() *7)
+      let eightBall = '';
+   
+      switch (randomNumber){
+         case 1:
+         eightBall = 'It is certain'
+         break;
+         case 2:
+         eightBall = 'It is decidedly so'
+         break;
+         case 3:
+         eightBall = 'Reply hazy try again'
+         break;
+         case 4:
+         eightBall = 'Cannot predict now'
+         break;
+         case 5:
+         eightBall = 'Do not count on it'
+         break;
+         case 6:
+         eightBall = 'My sources say no'
+         break;
+         case 7:
+         eightBall = 'Outlook not so good'
+        
+         break;
+         case 0:
+         eightBall = 'Signs point to yes'
+         break;
+       }
 
-switch (randomNumber){
-  case 1:
-  eightBall = 'It is certain'
-  break;
-  case 2:
-  eightBall = 'It is decidedly so'
-  break;
-  case 3:
-  eightBall = 'Reply hazy try again'
-  break;
-  case 4:
-  eightBall = 'Cannot predict now'
-  break;
-  case 5:
-  eightBall = 'Do not count on it'
-  break;
-  case 6:
-  eightBall = 'My sources say no'
-  break;
-  case 7:
-  eightBall = 'Outlook not so good'
-  break;
-  case 0:
-  eightBall = 'Signs point to yes'
-  break;
-}
+      response.innerHTML = eightBall;
+      response.classList.remove('hidden');
 
- if (userQuestion === ''){
-     console.log('Ask me anything')
-  } else {
-     console.log('Your question: ' + userQuestion)
-     console.log(eightBall)
-  }
+ }
+
+    input.addEventListener('keydown',(event)=>{
+      if (event.key === 'Enter'){
+         event.preventDefault();
+         assignResponse();
+      }
+   })
+
+
+
+
