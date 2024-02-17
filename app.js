80e31f9d1264eab08116885b15e7c7a6f77fdb0a
xPatiCiosek <patrycja_ciosek@hotmail.com>
2024-02-17 13:12:16 +0000

    added ability to save into Session storage

--- app.js
+++ app.js
@@ -1,56 +1,67 @@
-
-//let userQuestion = window.prompt('what is your question?')
-
 const response = document.querySelector('.response');
 const input = document.getElementById('input');
-const btn = document.getElementById('btn');
-
+const btnClear = document.getElementById('clear-table');
+const tableBody = document.getElementById("table-body");
+let data = sessionStorage;
 
  function assignResponse(){
-  
-      let randomNumber = Math.floor(Math.random() *7)
-      let eightBall = '';
-   
-      switch (randomNumber){
-         case 1:
-         eightBall = 'It is certain'
-         break;
-         case 2:
-         eightBall = 'It is decidedly so'
-         break;
-         case 3:
-         eightBall = 'Reply hazy try again'
-         break;
-         case 4:
-         eightBall = 'Cannot predict now'
-         break;
-         case 5:
-         eightBall = 'Do not count on it'
-         break;
-         case 6:
-         eightBall = 'My sources say no'
-         break;
-         case 7:
-         eightBall = 'Outlook not so good'
-        
-         break;
-         case 0:
-         eightBall = 'Signs point to yes'
-         break;
-       }
-
-      response.innerHTML = eightBall;
+     const responses = [
+          'It is certain', 'It is decidedly so', 'Reply hazy try again', 'Cannot predict now',
+          'Do not count on it', 'My sources say no', 'Outlook not so good', 'Signs point to yes'
+      ];
+      const randomNumber = Math.floor(Math.random() * responses.length);
+      const eightBall = responses[randomNumber];
+      response.textContent = eightBall;
       response.classList.remove('hidden');
-
+      return eightBall;
  }
 
- input.addEventListener('keydown',(event)=>{
-     if (event.key === 'Enter' && input.value !==''){
-          event.preventDefault();
-          assignResponse();
-          input.value='';
+ function createRow(key, value){
+     const row = document.createElement("tr");
+     const keyCell = document.createElement("td");
+     const valueCell = document.createElement("td");
+     keyCell.appendChild(document.createTextNode(key));
+     valueCell.appendChild(document.createTextNode(value));
+     row.appendChild(keyCell);
+     row.appendChild(valueCell);
+
+     tableBody.appendChild(row);
+ }
+
+ function setStorage(key, value){
+     data.setItem(key, value);
+     createRow(key,value);
+ }
+
+ function printTable(){
+     if (typeof(Storage) !== "undefined") {
+          const keys = Object.keys(data);
+          keys.forEach(function(key) {
+               createRow(key, data.getItem(key));
+          });
+     } else {
+          console.log("Sorry, your browser does not support Web Storage...");
      }
- })
+ }
+
+ function initBtn(){
+     input.addEventListener('keydown',(event)=>{
+          if (event.key === 'Enter' && input.value !==''){
+               event.preventDefault();
+               const value = assignResponse();
+               setStorage(input.value,value);
+               input.value='';               
+          }
+     })
+     btnClear.addEventListener('click', function(){
+          sessionStorage.clear();
+          tableBody.innerHTML = '';
+          alert("Your data has been deleted succesfully");         
+     });
+ }
+
+initBtn();
+printTable();
 
 
 
